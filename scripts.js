cd1fa125fd7532ffcaa4857dc68f8450d37f110c
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-05-16 15:51:37 +0900

    having progress bar updating, but can't stop updateProgress function

--- scripts.js
+++ scripts.js
@@ -1,20 +1,26 @@
-
 let playing = false;
-console.log(playing);
 
 function playVideo() {
-    console.log(this);
+    let progress;
     if (playing) {
         video.pause();
         this.innerHTML = "►";
+        clearInterval(progress);
     } else {
         video.play();
         this.innerHTML = "&#9616;&#9616;";
+        progress = setInterval(updateProgress, 250);
     }
     playing =! playing;
-    console.log(playing);
+}
+
+function updateProgress() {
+    let progress = ( video.currentTime / video.duration ) * 100;
+    progressBar.style = `flex-basis: ${progress}%`;
+    console.log(`${progress}%`);
 }
 
 const video = document.querySelector('video');
 const playButton = document.querySelector('.player__button');
 playButton.addEventListener('click', playVideo);
+const progressBar = document.querySelector('.progress__filled');
