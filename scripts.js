fe67d63668ee8c0f926c5b89d105b0ce56f0a7fa
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-05-16 18:49:47 +0900

    renamed function and variables to improve readability

--- scripts.js
+++ scripts.js
@@ -8,14 +8,14 @@ function playVideo() {
   } else {
     video.play();
     this.innerHTML = '&#9616;&#9616;';
-    progressing = setInterval(updateProgressBar, 250);
+    progressing = setInterval(updateProgressDisplay, 250);
   }
   playing = !playing;
 }
 
-function updateProgressBar() {
+function updateProgressDisplay() {
   let progress = (video.currentTime / video.duration) * 100;
-  progressBar.style = `flex-basis: ${progress}%`;
+  progressDisplay.style = `flex-basis: ${progress}%`;
 }
 
 function slideControls(e) {
@@ -29,12 +29,22 @@ function skipAround(e) {
   }
 }
 
+function jumpToPoint(e) {
+    lengthOfMovie = progressBar.getBoundingClientRect().width;
+    startOfMovie = progressBar.getBoundingClientRect().x;
+    jumpPoint = e.screenX;
+    percentOfMovie = ( (jumpPoint - startOfMovie) / lengthOfMovie) ;
+    video.currentTime = (video.duration * percentOfMovie);
+}
+
 const video = document.querySelector('video');
 const playButton = document.querySelector('.player__button');
-const progressBar = document.querySelector('.progress__filled');
+const progressBar = document.querySelector('.progress');
+const progressDisplay = document.querySelector('.progress__filled');
 const skipButtons = document.querySelectorAll('.player__button');
 const sliders = document.querySelectorAll('.player__slider');
 
+progressBar.addEventListener('mousedown', jumpToPoint);
 playButton.addEventListener('click', playVideo);
 skipButtons.forEach((button) => addEventListener('click', skipAround));
 sliders.forEach((slider) => addEventListener('input', slideControls));
