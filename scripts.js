5c08f50e9a4a43da87de476e6ba6909abcd1c0f7
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-05-16 16:54:34 +0900

    got video volume slider working

--- scripts.js
+++ scripts.js
@@ -4,22 +4,28 @@ function playVideo() {
     if (playing) {
         video.pause();
         this.innerHTML = "►";
-        clearInterval(progress);
+        clearInterval(progressing);
     } else {
         video.play();
         this.innerHTML = "&#9616;&#9616;";
-        progress = setInterval(updateProgress, 250);
+        progressing = setInterval(updateProgressBar, 250);
     }
     playing =! playing;
 }
 
-function updateProgress() {
+function updateProgressBar() {
     let progress = ( video.currentTime / video.duration ) * 100;
     progressBar.style = `flex-basis: ${progress}%`;
-    console.log(`${progress}%`);
+}
+
+function adjustVolume() {
+    video.volume = volumeControl.value;
+    console.log(video.volume);
 }
 
 const video = document.querySelector('video');
 const playButton = document.querySelector('.player__button');
 playButton.addEventListener('click', playVideo);
 const progressBar = document.querySelector('.progress__filled');
+const volumeControl = document.querySelector("div.player__controls input[name='volume']");
+volumeControl.addEventListener('change', adjustVolume);
