aed0b1c1821d6cc23e2fbfa6b993e4ba190da8f4
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-05-16 15:17:48 +0900

    toggling pause symbol

--- scripts.js
+++ scripts.js
@@ -3,10 +3,13 @@ let playing = false;
 console.log(playing);
 
 function playVideo() {
+    console.log(this);
     if (playing) {
         video.pause();
+        this.innerHTML = "►";
     } else {
         video.play();
+        this.innerHTML = "&#9616;&#9616;";
     }
     playing =! playing;
     console.log(playing);
